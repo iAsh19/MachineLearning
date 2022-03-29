3e66fbe41283a294a5ecf3b6f4f755b8c552e014
Ashar Malik <58248342+iAsh19@users.noreply.github.com>
2022-03-29 19:47:33 +0500

    Update imsciences_scraper.py

--- imsciences_scraper.py
+++ imsciences_scraper.py
@@ -1,19 +1,26 @@
+#The following code scrapes the latest news from https://imsciences.edu.pk
+
+#importing the required modules
 import requests
 from bs4 import BeautifulSoup
 
+#downloading the page to be scraped
 url = "https://imsciences.edu.pk/"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+
+#narrowing down and getting the information required
 table_div = soup.find("div", class_="col-sm-7")
 table_rows = table_div.find_all("tr")
 
+#printing the extracted information to the console
 for row in table_rows: 
     print(row.strong.get_text().strip())
     print(row.b.get_text().strip())
     print(str(row.a["href"]))
     print()
     
-#todo: some links in the output contains spaces. those links need to be manually pasted in the browser to work. how to deal with it.
+#todo: some links in the output contains spaces. those links need to be manually pasted in the browser to work. deal with it.
     
 
